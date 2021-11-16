e073d037c298ee7a79b05843049b93bb58415eaa
RayceWheat <RayceWheat@gmail.com>
2021-11-16 03:52:22 -0600

    initial seen movie routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,12 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const commentRoutes = require('./comment-routes');
+const postRoutes = require('./post-routes');
+const seenmovieRoutes = require('./seenmovie-routes');
 
 router.use('/users', userRoutes);
 router.use('/comments', commentRoutes);
+router.use('/posts', postRoutes);
+router.use('/seen-movies', seenmovieRoutes);
 
 module.exports = router;
