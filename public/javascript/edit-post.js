e588904cba76fb4f8e0178abab07cd6aed84e0b3
RayceWheat <RayceWheat@gmail.com>
2021-11-18 16:59:04 -0600

    added content route for post.put

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -13,7 +13,7 @@ async function editFormHandler(event) {
       const response = await fetch(`/api/posts/${id}`, {
         method: 'PUT',
         body: JSON.stringify({
-          post_id: id,
+          // post_id: id,
           title,
           content
         }),
